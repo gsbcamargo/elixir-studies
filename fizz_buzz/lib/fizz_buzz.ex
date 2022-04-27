21a9c09649c5febeea40b65811f6378ac695abfa
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-04-26 21:14:19 -0300

    added repositories

---
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -0,0 +1,9 @@
+defmodule FizzBuzz do
+  def build(file_name) do
+    file  = File.read(file_name)
+    handle_file_read(file)
+  end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:error, reason}), do: reason
+end
