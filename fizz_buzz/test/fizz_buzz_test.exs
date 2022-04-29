7b27e51f25130da351a5853b89b54cbf70acfe41
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-04-29 17:49:54 -0300

    added tests

--- fizz_buzz/test/fizz_buzz_test.exs
+++ fizz_buzz/test/fizz_buzz_test.exs
@@ -1,8 +1,17 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+
+    test "when a valid file is provided, a converted list is returned" do
+      expected_value = {:ok, [1, 2, :fizz, 4, :buzz, :fizz, 7, 8, :fizz, :buzz, 11, :fizz, 13, 14, :fizzbuzz, 16, 17, :fizz, 19, :buzz, :fizz, 22, 23, :fizz, :buzz, 26, :fizz, 28, 29, :fizzbuzz]}
+      assert FizzBuzz.build("assets/numbers.txt") == expected_value
+    end
+
+    test "when a invalid file is provided, an error is returned" do
+      expected_value = {:error, "Error reading the file: enoent"}
+      assert FizzBuzz.build("assets/invalid_file.txt") == expected_value
+    end
+
   end
 end
