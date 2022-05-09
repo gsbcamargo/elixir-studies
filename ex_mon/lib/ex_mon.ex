346d6eb286e7fa556d3ed7711d2fc6975e68c7c3
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-05-08 22:48:10 -0300

    refactoring ExMon module

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,13 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player
-  alias ExMon.Game
+  alias ExMon.{Game, Player}
+
+  @computer_name "ENIAC"
 
   def create_player(name, random_move, average_move, heal_move) do
     Player.build(name, random_move, average_move, heal_move)
   end
 
   def start_game(player) do
-    computer = create_player("ENIAC", :electric_bolt, :confusion, :defragment)
-    Game.start(computer, player)
+    @computer_name
+    |> create_player(:electric_bolt, :confusion, :defragment)
+    |> Game.start(player)
   end
 end
