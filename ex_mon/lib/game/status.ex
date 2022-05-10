65d185b92930fadb6de7f1429e23c92c4148f114
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-05-10 20:50:28 -0300

    adds right and wrong move input logic

--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -5,6 +5,9 @@ defmodule ExMon.Game.Status do
     IO.puts("\n =========== The game has started. ==========\n")
     IO.inspect(Game.info())
     IO.puts("-----------------------")
+  end
 
+  def print_wrong_move_message(move) do
+    IO.puts("\n =========== Invalid move: #{move} ==========\n")
   end
 end
